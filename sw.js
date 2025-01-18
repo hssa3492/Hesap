87e4c68167b61895c7c650bb99c6f508e9c965a5
hssa3492 <guvercinardasalm1234@gmail.com>
2025-01-18 11:41:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,20 @@
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('app-v2').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/icon.png',
-        // Tema için gereken ek kaynaklar
-        '/theme.js'
-      ]);
-    })
-  );
+    event.waitUntil(
+        caches.open('app-v2').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/icon.png',
+                '/theme.js'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
